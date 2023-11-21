9075d4825502e94f71cc7681ef1b95450c7b448b
kimdayeon37 <kdy37912@naver.com>
2023-11-21 13:13:48 +0900

    이미지 생성

--- src/components/Prompt/ResultImage.vue
+++ src/components/Prompt/ResultImage.vue
@@ -1,6 +1,6 @@
 <template>
     <br />
-    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" no-native-menu />
+    <q-img src="/src/assets/beautiful_sky.png" no-native-menu style="width:60%"/>
 </template>
 <script>
 //const imgUrl = ref("");
